e7687cd1617346b1528aabc326ca63c369116766
amitrrao <amith.nie@gmail.com>
2014-02-25 16:12:06 -0600

    latest

--- src/main/com/ca/im/ra/dbtest/ReadQueryInputFile.java
+++ src/main/com/ca/im/ra/dbtest/ReadQueryInputFile.java
@@ -2,6 +2,7 @@ package com.ca.im.ra.dbtest;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
@@ -11,4 +12,7 @@ public class ReadQueryInputFile {
 		return FileUtils.readFileToString(new File(fileName));
 	}
 
+	public List<String> readQueryFileReturnList(String fileName) throws IOException {
+		return FileUtils.readLines(new File(fileName));	
+	}
 }
--- src/test/com/ca/im/ra/dbtest/ReadQueryInputFileTest.java
+++ src/test/com/ca/im/ra/dbtest/ReadQueryInputFileTest.java
@@ -3,21 +3,39 @@ package com.ca.im.ra.dbtest;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
 public class ReadQueryInputFileTest {
 
+	private String fileName = "testquery.txt";
+	
 	@Test
 	public void readQueryFromTextFile() throws IOException {
 		// given there is a query input file
 		ReadQueryInputFile read = new ReadQueryInputFile();
 		
 		// when trying to read file
-		String actual = read.readQueryFile("testquery.txt");
+		
+		String actual = read.readQueryFile(fileName);
 		
 		// then should have query string
 		String expected = "select    protocol,    sum(inoctets + outoctets) Bytes    from protocol_traffic    where (router=167782434 and interface=9) and TimeStamp > 1392729900 - 900 and TimeStamp <= 1392758700 and protocol not in (0,4,1,2)    group by protocol    having Bytes > 0    order by Bytes desc    limit 12;";
 		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void readQueryFromTextFileReturnListStrings() throws IOException {
+		// given there is a query input file
+		ReadQueryInputFile read = new ReadQueryInputFile();
+		
+		// when trying to read file
+		List<String> actual = read.readQueryFileReturnList(fileName);
+		
+		// then should have list of strings
+		String expected = "select    protocol,    sum(inoctets + outoctets) Bytes    from protocol_traffic    where (router=167782434 and interface=9) and TimeStamp > 1392729900 - 900 and TimeStamp <= 1392758700 and protocol not in (0,4,1,2)    group by protocol    having Bytes > 0    order by Bytes desc    limit 12;";
+		assertEquals(expected, actual.get(0));
+		
 	}
 }
